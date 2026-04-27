b200b4a30cad36ef57e8587f3c636186b2e973ed
Pratyaksh Kumar <pratyakshkumar5678@gmail.com>
2026-04-27 14:50:10 +0530

    Update ConnectionDatabase.java

--- app_funding_v1-main/src/Connection/ConnectionDatabase.java
+++ app_funding_v1-main/src/Connection/ConnectionDatabase.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Connection;
 
 import GUI.Loading;
@@ -12,10 +8,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-/**
- *
- * @author Rizky
- */
+
 public class ConnectionDatabase {
     public Connection connect() {
         // Menampilkan Loading Screen
@@ -23,25 +16,24 @@ public class ConnectionDatabase {
         loadingScreen.setVisible(true);
         Connection conn = null;
         try {
-            // Load properties from application.properties file
+            
             Properties props = new Properties();
             
-            // Load the properties file from the classpath
             props.load(getClass().getClassLoader().getResourceAsStream("Config/application.properties"));
             
-            // Retrieve database connection properties
+           
             String url = props.getProperty("DB_URL");
             String user = props.getProperty("DB_USER");
             String password = props.getProperty("DB_PASSWORD");
             
-            // Create database connection
+            
             conn = DriverManager.getConnection(url, user, password);
-            // System.out.println("Database connection successful.");
+            
         } catch (IOException | SQLException e) {
             System.out.println("Database connection failed.");
             e.printStackTrace();
         }
-        // Menghilangkan Loading Screen
+
         loadingScreen.dispose();
         return conn;
     }
